367bca2e84219de86be2bba97d75ba13ca673c95
admin <admin@DESKTOP-VFVIQ95>
2021-05-23 23:04:23 +0530

    webservice

---
+++ WebService/src/main/java/webservice/impl/LoggerImpl.java
@@ -0,0 +1,11 @@
+package webservice.impl;
+
+import java.util.logging.Logger;
+
+public class LoggerImpl {
+	private static final Logger logger = Logger.getLogger("LoggerImpl.class");  
+	public static void main(String[] args) {
+		logger.info("message");
+	}
+
+}
